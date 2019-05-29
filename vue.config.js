0c0c985579be14b50e61f18f2c09207e8a375d8a
张良 <zhangliang15@meituan.com>
2019-05-29 20:52:50 +0800

    feat: 增加sourceMapUrl和pblicUrl

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,31 @@
-const path = require("path");
-const { spawn } = require("child_process");
+const path = require('path');
+const { spawn } = require('child_process');
+
+const { PUBLIC_URL, SOURCEMAP_PUBLIC_URL, GENERATE_SOURCEMAP } = process.env;
+const sourceMapSwitch = GENERATE_SOURCEMAP !== '0';
+
+const plugins = [];
+if (sourceMapSwitch) {
+  plugins.push(
+    new webpack.SourceMapDevToolPlugin({
+      filename: 'sourcemaps/[file].map',
+      publicPath: SOURCEMAP_PUBLIC_URL,
+    })
+  );
+}
 
 // https://cli.vuejs.org/zh/config
 module.exports = {
+  publicPath: PUBLIC_URL,
+
+  outputDir: path.resolve(__dirname, './build'),
+
+  transpileDependencies: [
+    /@sgfe\/eproduct/,
+    /@sgfe\/product-validate/,
+    /@sgfe\/reco-fe-theme-shangou-b\/lib/
+  ],
+
   css: {
     loaderOptions: {
       less: {
@@ -11,22 +34,18 @@ module.exports = {
     }
   },
 
-  outputDir: path.resolve(__dirname, './build'),
+  configureWebpack: {
+    plugins,
+  },
 
   chainWebpack: (config) => {
     config.module.rule('vue').uses.delete('cache-loader').end();
     config.module.rule('js').uses.delete('cache-loader').end();
   },
 
-  transpileDependencies: [
-    /@sgfe\/eproduct/,
-    /@sgfe\/product-validate/,
-    /@sgfe\/reco-fe-theme-shangou-b\/lib/
-  ],
-
   devServer: {
     port: 3030,
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     disableHostCheck: true,
     inline: true, // 实时刷新
     hot: true, // 使用热加载插件 HotModuleReplacementPlugin
@@ -34,16 +53,16 @@ module.exports = {
       warnings: true,
       errors: true
     },
-    stats: "minimal", // https://www.webpackjs.com/configuration/stats/
+    stats: 'minimal', // https://www.webpackjs.com/configuration/stats/
     before: app => {
       const proxy = process.env.PROXY;
       if (proxy) {
         app.use((req, res, next) => {
-          const apiPrefix = "/api/reuse/sc/product";
+          const apiPrefix = '/api/reuse/sc/product';
           let url = req.originalUrl || req.url;
           if (url.indexOf(apiPrefix) >= 0) {
-            if (url.indexOf("?") === -1) {
-              url += "?";
+            if (url.indexOf('?') === -1) {
+              url += '?';
             }
             url += `&u=2137588&c=reuse_M_queenbee&n=luodetao`;
             url = url.replace(apiPrefix, `/${proxy}/api/reuse/sc/product`);
@@ -57,36 +76,36 @@ module.exports = {
       // 使用nodemon启动mockServer
       // nodemon('./_mock/server --watch _mock')
       const cp = spawn(
-        "node",
-        ["./node_modules/.bin/nodemon", "./_mock/server", "--watch", "_mock"],
+        'node',
+        ['./node_modules/.bin/nodemon', './_mock/server', '--watch', '_mock'],
         {
-          stdio: ["pipe", "pipe", "pipe", "ipc"]
+          stdio: ['pipe', 'pipe', 'pipe', 'ipc']
         }
       );
-      process.on("SIGINT", () => {
-        cp.emit("close");
+      process.on('SIGINT', () => {
+        cp.emit('close');
         process.exit(0);
       });
     },
     proxy: {
-      "/api": {
-        target: "http://localhost:10010"
+      '/api': {
+        target: 'http://localhost:10010'
       },
-      "/dev/api/reuse/sc/product": {
-        target: "http://eproductapi.sc.waimai.dev.sankuai.com",
-        pathRewrite: { "^/dev/api": "" },
+      '/dev/api/reuse/sc/product': {
+        target: 'http://eproductapi.sc.waimai.dev.sankuai.com',
+        pathRewrite: { '^/dev/api': '' },
         changeOrigin: true,
         secure: false
       },
-      "/test/api/reuse/sc/product": {
-        target: "http://eproductapi.sc.waimai.test.sankuai.com",
-        pathRewrite: { "^/test/api": "" },
+      '/test/api/reuse/sc/product': {
+        target: 'http://eproductapi.sc.waimai.test.sankuai.com',
+        pathRewrite: { '^/test/api': '' },
         changeOrigin: true,
         secure: false
       },
-      "/st/api/reuse/sc/product": {
-        target: "http://eproductapi.sc.waimai.st.sankuai.com",
-        pathRewrite: { "^/st/api": "" },
+      '/st/api/reuse/sc/product': {
+        target: 'http://eproductapi.sc.waimai.st.sankuai.com',
+        pathRewrite: { '^/st/api': '' },
         changeOrigin: true,
         secure: false
       }
@@ -94,9 +113,9 @@ module.exports = {
   },
 
   pluginOptions: {
-    "style-resources-loader": {
-      preProcessor: "less",
-      patterns: [path.resolve(__dirname, "src/styles/var.less")]
+    'style-resources-loader': {
+      preProcessor: 'less',
+      patterns: [path.resolve(__dirname, 'src/styles/var.less')]
     }
   }
 };
