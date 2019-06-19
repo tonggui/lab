23d9776a23320333c17a081b614a632e738fbf0f
张一 <zhangyi34@meituan.com>
2019-06-19 14:26:04 +0800

    视频api数据转换

--- src/data/api/videoApi.js
+++ src/data/api/videoApi.js
@@ -1,11 +1,15 @@
 import apiClient from './client'
+import { convertProductVideoFromServer } from './helpers/video'
+
+const isLocal = process.env.NODE_ENV === 'development'
+
+// 视频文件上传接口
+export const uploadUrl = `${isLocal ? '/api' : ''}/reuse/sc/product/retail/uploadTool/w/uploadToVideoSpace`
 
 export const fetchVideoList = params =>
-  apiClient.post('/reuse/product/food/video/r/list', params, {
+  apiClient.post('/reuse/sc/product/retail/video/r/list', params, {
     baseURL: '/api'
   }).then(data => {
-    const { totalNum = 0, usage = 0, wmProductVideoVoList: list = [] } = data || {}
-    return {
-      totalNum, usage, list
-    }
+    data.list = (data.wmProductVideoVoList || []).map(convertProductVideoFromServer)
+    return data
   })
