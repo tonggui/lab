3a014902ad1d6db39e433cfc400fe4054afcdf1a
张一 <zhangyi34@meituan.com>
2019-06-26 21:48:57 +0800

    rulecontroller weaver.js

---
+++ src/components/dynamic-form/rule-controller.js
@@ -0,0 +1,43 @@
+import { isFunction } from 'lodash'
+import Rule from './Rule'
+
+class RuleController {
+  constructor ({
+    config,
+    execContext,
+    configChangeHandler
+  }) {
+    this.config = config
+    this.execContext = execContext
+    this.bindEvents()
+    this.configChangeHandler = configChangeHandler
+    if (this.config.rules) this.config.rules.forEach(rule => new Rule(rule, this))
+  }
+
+  bindEvents () {
+    Object.keys(this.config.events || {}).forEach((eventName) => {
+      this.config.events[eventName] = this.config.events[eventName].bind(this.execContext)
+    })
+    // 将options中的方法的this指向execContext
+    Object.keys(this.config.options || {}).forEach((k) => {
+      if (isFunction(this.config.options[k])) {
+        this.config.options[k] = this.config.options[k].bind(this.execContext)
+      }
+    })
+  }
+
+  getExecContext () {
+    return this.execContext
+  }
+
+  setResultVal (key, value) {
+    if (!key) return
+    // 如果目标是value，则同时更新formData对应的值
+    if (key === 'value' && (this.config.key in this.execContext.formData)) {
+      this.execContext.formData[this.config.key] = value
+    }
+    this.configChangeHandler(key, value)
+  }
+}
+
+export default RuleController
