32fb84ed10f1cbfc36659c7316f3bfe2d144c56a
张良 <zhangliang15@meituan.com>
2019-06-27 22:39:49 +0800

    feat: merge from master

---
+++ src/data/repos/productRepository.js
@@ -0,0 +1,15 @@
+/*
+ * @description
+ *   Please write the productRepository script's description
+ * @author rdshoep(rdshoep@126.com)
+ *   http://www.rdshoep.com/
+ * @version
+ *   1.0.0(2018/5/20)
+ */
+
+export {
+  fetchProductList,
+  fetchRecycleProductList,
+  cleanRecycleBin,
+  recoverRecycleSpus
+} from '../api/productApi'
