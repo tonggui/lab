e162aaacaeb1f77ef2cf817992ba535062886056
张一 <zhangyi34@meituan.com>
2019-10-15 17:32:43 +0800

    feat: 补充mock

---
+++ _mock/api/poi/fieldVisibleConfig.js
@@ -0,0 +1,15 @@
+/**
+ * @url reuse/sc/product/retail/r/getFieldVisibleConfig
+ */
+module.exports = function () {
+  return {
+    data: {
+      boxPrice: false,
+      descProduct: true,
+      message: "",
+      shippingTime: true
+    },
+    code: 0,
+    msg: ''
+  };
+};
---
+++ _mock/api/product/saveOrUpdateWithCategoryAttrs.js
@@ -0,0 +1,11 @@
+/**
+ * @url /reuse/sc/product/shangou/w/saveOrUpdateProduct
+ */
+module.exports = function () {
+  return {
+    data: {},
+    // 'code|1': [1013, 1014, 1015, 9101, 9102, 9103],
+    code: 1013,
+    msg: '保存失败啦啦啦啦啦啦'
+  };
+};
