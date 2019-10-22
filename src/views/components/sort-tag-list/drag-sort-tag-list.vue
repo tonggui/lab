b67db0c6b450dd34904a27ec83a9612607d232a4
weiru <weiru@meituan.com>
2019-10-22 18:01:09 +0800

    feat：未分类 拖拽问题

--- src/views/components/sort-tag-list/drag-sort-tag-list.vue
+++ src/views/components/sort-tag-list/drag-sort-tag-list.vue
@@ -1,24 +1,35 @@
 <template>
-  <TagTree
-    class="drag-sort-tag-list"
-    :labelInValue="labelInValue"
-    :value="tagId"
-    :dataSource="tagList"
-    :showTopTime="false"
-    @select="$listeners.select"
-    @expand="$listeners.expand"
-    :expandList="expandList"
-    :draggable="true"
-    draggable-handle=".handle"
-    @sort="handleSort"
-    transition-name=""
-  >
-    <template v-slot:node-extra="{item, index}">
-      <span class="drag-sort-icon handle" v-if="!item.isUnCategorized">
-        <Icon local="drag" size=18></Icon>
-      </span>
-    </template>
-  </TagTree>
+  <div>
+    <!-- 未分类始终置顶 -->
+    <TagTree
+      v-if="unCategorizedList.length > 0"
+      :labelInValue="labelInValue"
+      :value="tagId"
+      :dataSource="unCategorizedList"
+      :show-top-time="false"
+      @select="$listeners.select"
+    />
+    <TagTree
+      class="drag-sort-tag-list"
+      :labelInValue="labelInValue"
+      :value="tagId"
+      :dataSource="dataSource"
+      :show-top-time="false"
+      @select="$listeners.select"
+      @expand="$listeners.expand"
+      :expandList="expandList"
+      :draggable="true"
+      draggable-handle=".handle"
+      @sort="handleSort"
+      transition-name=""
+    >
+      <template v-slot:node-extra="{item, index}">
+        <span class="drag-sort-icon handle" v-if="!item.isUnCategorized">
+          <Icon local="drag" size=18></Icon>
+        </span>
+      </template>
+    </TagTree>
+  </div>
 </template>
 <script>
   import TagTree from '@components/tag-tree'
@@ -35,12 +46,27 @@
       tagId: Number,
       expandList: Array
     },
+    computed: {
+      unCategorizedList () {
+        return this.tagList.filter(item => item.isUnCategorized)
+      },
+      dataSource () {
+        return this.tagList.filter(item => !item.isUnCategorized)
+      }
+    },
     components: {
       TagTree
     },
     methods: {
       handleSort (list, tag, sortList) {
         lx.mc({ bid: 'b_shangou_online_e_x5yb9btj_mc' })
+        // 未分类位置不能动
+        if (this.unCategorizedList.length > 0) {
+          list = [...this.unCategorizedList, ...list]
+          if (tag.level === 0) {
+            sortList = [...this.unCategorizedList, ...sortList]
+          }
+        }
         this.$emit('change', list, tag, sortList)
       }
     }
