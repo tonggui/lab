1df93590bc15f83ecbc08cb8ef4106b77904fc5c
张一 <zhangyi34@meituan.com>
2019-11-01 18:06:40 +0800

    fix: 删除多余log

--- src/data/helper/product/base/convertFromServer.ts
+++ src/data/helper/product/base/convertFromServer.ts
@@ -200,7 +200,6 @@ export const convertProductInfoWithPagination = (data: any, requestQuery) => {
     spuSortType // 1: 手动排序 2: 智能排序
   } = (data || {}) as any;
   const { pagination, statusList } = requestQuery
-  console.log('fetchGetProductInfoList:', statusList)
   return {
     list: convertProductInfoList(productList),
     pagination: {
