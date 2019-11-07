5a8286d13fcfab63af497ccd182a611bc59f1414
weiru <weiru@meituan.com>
2019-11-07 19:24:11 +0800

    feat: 修改校验

--- src/views/components/product-sku-edit/edit/inline/price.vue
+++ src/views/components/product-sku-edit/edit/inline/price.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="merchant-product-sku-edit-price" :class="{ error }">
-    <div>
-      <InputNumber :value="value" :max="30000" :min="0" :precision="2" @on-change="handleChange" size="small" clearable>
-        <template slot="prefix">¥</template>
-      </InputNumber>
-    </div>
-    <div class="error" v-show="error">{{ error }}</div>
+    <UnitNumber unit="¥">
+      <div>
+        <InputNumber number v-model="selfValue" :max="30000" :min="0" size="small" clearable />
+        <div class="error" v-show="error">{{ error }}</div>
+      </div>
+    </UnitNumber>
   </div>
 </template>
 <script>
+  import UnitNumber from '@components/unit-number'
+
   export default {
     name: 'merchant-product-sku-edit-stock',
     props: {
@@ -17,17 +19,36 @@
     },
     data () {
       return {
-        error: ''
+        error: '',
+        selfValue: this.value
       }
     },
     watch: {
       value (value) {
-        this.error = this.validator(value)
+        if (this.selfValue !== value) {
+          this.selfValue = value
+        }
+      },
+      selfValue (newValue, oldValue) {
+        if (newValue) {
+          const regx = /^(([1-9]\d*)|0)(\.\d{0,2})?$/
+          if (!regx.test(newValue.toString())) {
+            this.$nextTick(() => {
+              this.selfValue = oldValue
+            })
+            return
+          }
+        }
+        this.error = this.validator(newValue)
+        this.handleChange(newValue)
       }
     },
+    components: {
+      UnitNumber
+    },
     methods: {
-      handleChange (e) {
-        const value = e.target.value
+      handleChange (value) {
+        this.selfValue = value
         this.$emit('change', value)
         this.$emit('input', value)
       }
@@ -40,13 +61,19 @@
   .merchant-product-sku-edit-price {
     text-align: left;
     position: relative;
+    /deep/ .boo-input-number {
+      width: 100%;
+    }
+    /deep/ .boo-input-number-handler-wrap {
+      display: none;
+    }
     /deep/ .boo-input-wrapper-small .boo-input-prefix {
       font-size: @font-size-small;
       line-height: 28px;
       color: @text-tip-color;
     }
     &.error {
-      /deep/ .boo-input {
+      /deep/ .boo-input-number {
         border: 1px solid @error-color;
       }
     }
