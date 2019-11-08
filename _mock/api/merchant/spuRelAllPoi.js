6557446b367f9885aa99caaf4ac5e55c94bf9898
weiru <weiru@meituan.com>
2019-11-08 17:20:19 +0800

    feat: 添加商品关联门店 全选所有 mock

---
+++ _mock/api/merchant/spuRelAllPoi.js
@@ -0,0 +1,12 @@
+/**
+ * @url /reuse/sc/product/hqcc/r/listAllRelPoi
+ */
+module.exports = {
+  code: 0,
+  message: '',
+  "data|20": [{
+    "poiId|+1": 0,
+    name: '@name',
+    address: '@address'
+  }] 
+}
