7444cf3d5645a24dbe27126c04127be2c3843f2d
weiru <weiru@meituan.com>
2019-12-11 16:24:08 +0800

    feat: 药品品类判断 修改

--- src/module/category/helper.js
+++ src/module/category/helper.js
@@ -1,1 +1,7 @@
-export const isNormalMedicine = category => category.pid === 22 && category.id !== 4012
+/**
+ * 药品品类判断
+ * 一级经营品类 22，二级经营品类 [179， 180， 181] 中的一个
+ * 只有179(OTC),180(中药),181(保健品)走药品逻辑，受到药品控制
+ * @param {*} category
+ */
+export const isNormalMedicine = category => category.pid === 22 && [179, 180, 181].includes(category.id)
