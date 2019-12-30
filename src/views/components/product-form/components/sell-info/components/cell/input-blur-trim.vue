83d3773af19ada4c40618b2a44b14ddd706b2805
张一 <zhangyi34@meituan.com>
2019-12-30 20:44:53 +0800

    fix: 补充disabled属性

--- src/views/components/product-form/components/sell-info/components/cell/input-blur-trim.vue
+++ src/views/components/product-form/components/sell-info/components/cell/input-blur-trim.vue
@@ -1,11 +1,12 @@
 <template>
-  <Input :value="value" v-bind="$attrs" v-on="listeners" @on-blur="handleBlur" @on-change="handleChange" />
+  <Input :value="value" :disabled="disabled" v-bind="$attrs" v-on="listeners" @on-blur="handleBlur" @on-change="handleChange" />
 </template>
 <script>
   export default {
     name: 'input-blur-trim',
     props: {
-      value: [Number, String]
+      value: [Number, String],
+      disabled: Boolean
     },
     computed: {
       listeners () {
