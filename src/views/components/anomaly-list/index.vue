765914dd18e39650986137d36aa35db1fa7f6492
郭丽 <guoli06@meituan.com>
2020-02-03 14:45:17 +0800

    feat: 增加anomaly-list的loading状态

--- src/views/components/anomaly-list/index.vue
+++ src/views/components/anomaly-list/index.vue
@@ -11,20 +11,28 @@
         <span class="header-opr">优化操作</span>
       </div>
     </div>
-    <AnomalyCard v-for="(data, index) in list"
-                 :key="index"
-                 :anomaly-type="anomalyType"
-                 :tag-list="tagList"
-                 :data="data"
-                 :poi-id="poiId">
-    </AnomalyCard>
-    <div class="page-container">
-      <Pagination v-if="list.length" :pagination="pagination" @on-change="handlePageChange" />
+    <div class="anomaly-list-container">
+      <div v-if="list.length && !loading" class="list-and-pagination">
+        <AnomalyCard v-for="(data, index) in list"
+                     :key="index"
+                     :anomaly-type="anomalyType"
+                     :tag-list="tagList"
+                     :data="data"
+                     :poi-id="poiId">
+        </AnomalyCard>
+        <div class="page-container">
+          <Pagination v-if="list.length" :pagination="pagination" @on-change="handlePageChange" />
+        </div>
+      </div>
+      <div v-if="loading" class="anomaly-list-loading">
+        <Loading size="large" class="anomaly-list-loading-icon" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+  import Loading from '@/components/loading/flash-loading'
   import AnomalyCard from './anomaly-card'
   import Pagination from '@/components/pagination'
   import { fetchGetTagList } from '@/data/repos/category'
@@ -34,6 +42,7 @@
   export default {
     name: 'anomaly-list',
     components: {
+      Loading,
       AnomalyCard,
       Pagination
     },
@@ -46,6 +55,7 @@
     data () {
       return {
         TYPE,
+        loading: false,
         tagList: [],
         list: [],
         pagination: {
@@ -71,15 +81,23 @@
       },
 
       // 获取异常列表
-      getAnomalyList (anomalyType = this.anomalyType) {
-        const type = anomalyType === TYPE.PRICE_ANOMALY ? 1 : (anomalyType === TYPE.STOCK_ANOMALY ? 2 : 3)
-        fetchGetAnomalyList(this.poiId, type, this.pagination)
-          .then(data => {
-            this.list = data.list
-            this.pagination.total = data.page.totalCount
-          }).catch(err => {
-            this.$Message.error(err.message || err)
-          })
+      async getAnomalyList (anomalyType = this.anomalyType) {
+        try {
+          this.loading = true
+          const type = anomalyType === TYPE.PRICE_ANOMALY ? 1 : (anomalyType === TYPE.STOCK_ANOMALY ? 2 : 3)
+          await fetchGetAnomalyList(this.poiId, type, this.pagination)
+            .then(data => {
+              this.list = data.list
+              this.pagination.total = data.page.totalCount
+            })
+            .catch(err => {
+              this.$Message.error(err.message || err)
+            })
+        } catch (e) {
+          console.error(e)
+        } finally {
+          this.loading = false
+        }
       },
 
       handlePageChange (page) {
@@ -133,11 +151,33 @@
         }
       }
     }
+    .anomaly-list-container {
+      position: relative;
+      width: 100%;
+      height: 100%;
+      min-height: 300px;
+      background-color: rgba(255, 255, 255, .9);
+    }
     .page-container {
       display: flex;
       justify-content: flex-end;
       align-items: center;
       padding: 10px 20px 30px 0;
+    }
+    .anomaly-list-loading {
+      position: absolute;
+      top: 0;
+      left: 0;
+      z-index: 10;
+      width: 100%;
+      height: 100%;
+      text-align: center;
+      &-icon {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+      }
     }
   }
 </style>
