0e4a767249df25222af5bf38b4820444ff3d0ab8
weiru <weiru@meituan.com>
2020-02-14 23:19:30 +0800

    feat: 代码优化

--- src/common/local-storage.js
+++ src/common/local-storage.js
@@ -26,7 +26,8 @@ export const KEYS = {
   // 从商品库创建tab
   SP_LIST_TAB: 'spListTab',
   UNAPPROVE_PRODUCT_ENTRANCE_TIP: 'unApproveProductTip',
-  TABLE_HEADER_REL_POI_TIP: 'tableHeaderRelPoiTip'
+  TABLE_HEADER_REL_POI_TIP: 'tableHeaderRelPoiTip',
+  SET_AUTO_CLEAR_STOCK_CONFIG: 'setAutoClearStockConfig'
 }
 
 const storage = {}
