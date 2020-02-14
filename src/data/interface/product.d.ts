dc71c53d7a9a73806ebeaf0e0850bd3f64d99b40
weiru <weiru@meituan.com>
2020-02-14 20:54:50 +0800

    feat：修改接口字读

--- src/data/interface/product.d.ts
+++ src/data/interface/product.d.ts
@@ -91,7 +91,7 @@ declare interface ProductInfo {
     tip: string; // 列表中展示的提示信息
     message: string; // 点击提示后modal中的展示文案
   },
-  tagIdList: number[]; // 商品属于的分类id
+  tagList: number[]; // 商品属于的分类id
   errorTip: string;
   locked?: boolean; // 字段是否锁定
   stockoutAutoClearStock: boolean; // 是否设置缺货库存自动清零
