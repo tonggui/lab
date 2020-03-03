bd44164122bbc7d028c4b857de156505f66aff3c
weiru <weiru@meituan.com>
2020-03-03 12:52:51 +0800

    fix: xls文件匹配问题

--- src/views/batch-management/batch-create/components/excel-create/excel-create.vue
+++ src/views/batch-management/batch-create/components/excel-create/excel-create.vue
@@ -10,7 +10,7 @@
     </OrderFormItem>
     <OrderFormItem label="上传Excel表格" key="file" :index="index + 3">
       <Checkbox v-if="supportUseSpImage" v-model="isUsePicBySp">使用商品库图片</Checkbox>
-      <FileUpload @submit="handleSubmit" accept=".cvs,application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" />
+      <FileUpload @submit="handleSubmit" accept=".cvs,.xls,application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" />
     </OrderFormItem>
     <AgreementModal mode="view" v-model="modalVisible"></AgreementModal>
   </div>
