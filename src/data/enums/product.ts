bf321095d158534a925c1d933ed7d904cb5d965e
张一 <zhangyi34@meituan.com>
2020-03-05 15:37:23 +0800

    feat: 审核状态增加注释

--- src/data/enums/product.ts
+++ src/data/enums/product.ts
@@ -77,10 +77,10 @@ export enum SKU_EDIT_TYPE {
 }
 // 审核枚举值
 export enum PRODUCT_AUDIT_STATUS {
-  UNAUDIT = 0,
-  AUDITING = 1,
-  AUDIT_APPROVED = 2,
-  AUDIT_REJECTED = 3,
-  AUDIT_CORRECTION_REJECTED = 4,
-  AUDIT_REVOCATION = 5,
+  UNAUDIT = 0, // 未审核
+  AUDITING = 1, // 审核中
+  AUDIT_APPROVED = 2, // 审核通过
+  AUDIT_REJECTED = 3, // 审核驳回
+  AUDIT_CORRECTION_REJECTED = 4, // 纠错审核驳回，纠错时指：1.商家在初始提交的审核是由于UPC存在+修改关键字段（UPC、类目、关键类目属性）所致。2. 审核通过后的商品修改关键字段（UPC、类目、关键类目属性）所致
+  AUDIT_REVOCATION = 5, // 审核撤销
 }
