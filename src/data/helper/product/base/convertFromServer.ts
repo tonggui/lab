a0e50e922ac4065495b48d04ad575b734eed3f89
weiru <weiru@meituan.com>
2020-03-11 14:43:34 +0800

    feat: 接口处理

--- src/data/helper/product/base/convertFromServer.ts
+++ src/data/helper/product/base/convertFromServer.ts
@@ -125,16 +125,18 @@ export const convertProductInfo = (product: any, validationConfigMap, isAuditPro
     tip: '',
     message: '',
   };
-  (labels || []).forEach((label) => {
-    const { id, groupName } = label
-    if (id === 16) {
-      errorTip = '超出经营范围，禁止售卖';
-    } else if (id === 15) {
-      qualification.exist = false
-      qualification.tip = '需补充资质后方可售卖';
-      qualification.message = groupName || '需补充资质后方可售卖';
-    }
-  })
+  if (!isAuditProductList) {
+    (labels || []).forEach((label) => {
+      const { id, groupName } = label
+      if (id === 16) {
+        errorTip = '超出经营范围，禁止售卖';
+      } else if (id === 15) {
+        qualification.exist = false
+        qualification.tip = '需补充资质后方可售卖';
+        qualification.message = groupName || '需补充资质后方可售卖';
+      }
+    })
+  }
   const node: ProductInfo = {
     id,
     name,
