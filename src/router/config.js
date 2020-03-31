c03bebc7b0d623ce5719b76ff3e4d6dd6a24191e
weiru <weiru@meituan.com>
2020-03-31 14:06:45 +0800

    feat：8.0.0 pv 埋点

--- src/router/config.js
+++ src/router/config.js
@@ -145,7 +145,7 @@ const routeList = [
       ),
     meta: {
       title: '商品审核',
-      cid: '' // TODO 埋点
+      cid: 'c_shangou_online_e_xreb5mx3' // TODO 埋点
     }
   },
   {
@@ -171,7 +171,7 @@ const routeList = [
       ),
     meta: {
       title: '商品审核详情',
-      cid: '' // TODO 埋点
+      cid: 'c_shangou_online_e_rrpt94dt' // TODO 埋点
     }
   },
   {
