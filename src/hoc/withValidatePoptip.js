f58025063604a5ecaf27972a3cd65723a13e6d09
weiru <weiru@meituan.com>
2020-04-09 16:41:20 +0800

    feat：气泡错误提示 高阶组件

---
+++ src/hoc/withValidatePoptip.js
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import { getName } from './helper'
+import Tooltip from '@/components/tooltip'
+
+export default (WrapperComponent, { duraution = 3000, transfer = false } = {}) => Vue.extend({
+  name: getName('with-validate-poptip', WrapperComponent),
+  data () {
+    return {
+      error: '',
+      show: false
+    }
+  },
+  watch: {
+    error () {
+      clearTimeout(this.timer)
+      this.timer = setTimeout(() => {
+        this.show = false
+      }, duraution)
+    }
+  },
+  created () {
+    this.timer = null
+  },
+  methods: {
+    handleError (error) {
+      this.show = !!error
+      this.error = error
+      this.$emit('on-error', error)
+    }
+  },
+  render (h) {
+    const { 'on-error': onError, ...rest } = this.$listeners
+    return h(Tooltip, {
+      attrs: {
+        disabled: !this.show,
+        always: true,
+        content: this.error,
+        placement: 'top-start',
+        transfer
+      }
+    }, [h(WrapperComponent, {
+      props: {
+        showErrorTip: false
+      },
+      attrs: { ...this.$attrs },
+      on: {
+        ...rest,
+        'on-error': this.handleError
+      }
+    })])
+  }
+})
