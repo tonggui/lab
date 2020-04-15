a60fed0941cfa4fe6347cb3beee6ea82d03036eb
张良 <zhangliang15@meituan.com>
2020-04-15 12:23:31 +0800

    feat: 修复数据转化异常

--- src/data/api/medicineSpAudit.ts
+++ src/data/api/medicineSpAudit.ts
@@ -120,10 +120,10 @@ export const spAuditDetail = async ({
           value = attrValueItem.extension
           break
         case VALUE_TYPE.SINGLE_SELECT:
-          value = +attrValueItem.valudId || 0
+          value = +attrValueItem.valueId || 0
           break
         case VALUE_TYPE.MULTI_SELECT:
-          value = _.map(_.split(attrValueItem.valudId, ','), v => +v || 0)
+          value = _.map(_.split(attrValueItem.valueId, ','), v => +v || 0)
           break
       }
       valueMap[categoryAttrItem.id] = value
