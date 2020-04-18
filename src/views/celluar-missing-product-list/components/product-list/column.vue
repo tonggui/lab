b8113dbf1e00d802c0946012e38e307d503e4ef5
weiru <weiru@meituan.com>
2020-04-18 16:22:41 +0800

    feat: 月售逻辑修改

--- src/views/celluar-missing-product-list/components/product-list/column.vue
+++ src/views/celluar-missing-product-list/components/product-list/column.vue
@@ -97,19 +97,12 @@
             */
             let description = ''
             if (this.type === TAB.EXIST) {
-              if (row.monthSale) {
-                const monthSale = row.monthSale > 999 ? '999+' : row.monthSale
-                description = `月售${monthSale}`
-              } else if (row.isSp) {
-                description = row.upcCode || ''
-              }
+              description = `月售${row.monthSale || 0}`
+            } else if (nameEditable) {
+              const example = this.productNameExample || ''
+              description = example && `参考格式 ${example}`
             } else {
-              if (nameEditable) {
-                const example = this.productNameExample || ''
-                description = example && `参考格式 ${example}`
-              } else {
-                description = row.upcCode || ''
-              }
+              description = row.upcCode || ''
             }
             const handleChange = (name) => this.triggerModify({ name }, row)
             return (
