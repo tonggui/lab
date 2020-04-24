57c8b80bf767c58996bae60c7a6b856b6fa69426
weiru <weiru@meituan.com>
2020-04-24 20:06:12 +0800

    feat: 修改

--- src/views/celluar-missing-product-list/celluar-missing-product-list.vue
+++ src/views/celluar-missing-product-list/celluar-missing-product-list.vue
@@ -21,7 +21,7 @@
       <Empty v-else-if="searchEmpty" description="暂无搜索结果" class="celluar-product-list-page-search-empty" />
       <div v-else class="celluar-product-list-page-tabs">
         <Tabs :value="activeTab" @on-click="handleTabChange" class="tab">
-          <TabPane v-for="tab in tabList" :name="tab.id" :label="tab.label" :key="tab.id">
+          <TabPane v-for="(tab, index) in tabList" :name="tab.id" :label="tab.label" :key="index">
             <component :is="getComponent(tab.id)" :tag-list="tagList" @after-put-on="handlePutOn" />
           </TabPane>
         </Tabs>
