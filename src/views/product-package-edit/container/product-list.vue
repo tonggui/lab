05059f74ddf4302d6d35a4c9bdc68ebf7878e712
danzewei <danzewei@meituan.com>
2020-07-06 15:53:44 +0800

    feat: 商品选择组件优化，样式调整

---
+++ src/views/product-package-edit/container/product-list.vue
@@ -0,0 +1,148 @@
+<template>
+  <Modal :value="value" :width="1000" :mask-closable="false" class-name="product-list-modal-container" @on-cancel="handleCancel">
+    <ProductListPage>
+      <div slot="header" class="product-list-header">组合商品选择</div>
+      <ProductSelect
+        slot="content"
+        :tableSource="dataSource"
+        :loading="loading"
+        :tagList="tagList"
+        :selectedTagId="selectedTagId"
+        :selectedList="selectedList"
+        :pagination="pagination"
+        :maxCount="maxCount"
+        @on-page-change="handlePageChange"
+        @on-select="handleSelect"
+        @on-de-select="handleDeSelect"
+        @on-tag-change="handleTagChange"
+      >
+        <ProductSearch slot="header-right" @on-search="handleSearch" :searchValue="keyword" />
+      </ProductSelect>
+    </ProductListPage>
+    <Button slot="footer" type="primary" @click="handleOk">确定</Button>
+  </Modal>
+</template>
+
+<script>
+  import ProductListPage from '@/views/components/layout/product-list-page'
+  import ProductSelect from '@/views/components/product-select/product-select'
+  import ProductSearch from '../components/product-search'
+  import { fetchGetProductInfoList } from '@/data/repos/product'
+  import { fetchGetPoiTagInfo } from '@/data/repos/category'
+  const MAX_COUNT = 5
+  export default {
+    name: 'product-list',
+    props: {
+      value: {
+        type: Boolean,
+        default: false
+      },
+      selectedProductList: Array
+    },
+    components: {
+      ProductSearch,
+      ProductSelect,
+      ProductListPage
+    },
+    data: () => {
+      return {
+        maxCount: MAX_COUNT,
+        dataSource: [],
+        selectedTagId: 0,
+        tagList: [],
+        keyword: '',
+        selectedList: [],
+        loading: false,
+        pagination: {
+          pageNum: 1,
+          pageSize: 20,
+          total: 0,
+          showTotal: true,
+          showSizer: true,
+          pageSizeOpts: [20, 50, 100]
+        }
+      }
+    },
+    watch: {
+      value (val) {
+        if (val) {
+          this.getData()
+          this.selectedList = [...this.selectedProductList]
+        }
+      }
+    },
+    methods: {
+      handleOk () {
+        this.$emit('on-ok', this.selectedList)
+      },
+      handleCancel () {
+        this.selectedList = []
+        this.$emit('on-cancel')
+      },
+      handleSelect (val) {
+        this.selectedList.push(...val)
+      },
+      handleDeSelect (val) {
+        const result = []
+        this.selectedList.forEach(item => {
+          if (!val.some(it => it.id === item.id)) {
+            result.push(item)
+          }
+        })
+        this.selectedList = result
+      },
+      handleTagChange (tagId) {
+        this.selectedTagId = tagId
+        this.getDataSource()
+      },
+      handleSearch (keyword) {
+        this.keyword = keyword.name
+        this.getDataSource()
+      },
+      handlePageChange (pagination) {
+        this.pagination = pagination
+        this.getDataSource()
+      },
+      getTagList () {
+        fetchGetPoiTagInfo().then(res => {
+          this.tagList = res.tagList
+        })
+      },
+      getDataSource () {
+        this.loading = true
+        fetchGetProductInfoList({ tagId: this.selectedTagId, keyword: this.keyword }, this.pagination).then(res => {
+          this.loading = false
+          const { pagination, list } = res
+          this.dataSource = list
+          this.pagination = pagination
+        }).catch(err => {
+          console.log(err)
+          this.loading = false
+        })
+      },
+      getData () {
+        this.getTagList()
+        this.getDataSource()
+      }
+    }
+  }
+</script>
+
+<style lang="less">
+  .product-list-modal-container {
+    /deep/ .boo-modal {
+      min-height: 400px !important;
+      max-height: 500px !important;
+      /deep/ .boo-modal-content {
+        min-height: 100%;
+        max-height: 100%;
+        /deep/ .boo-modal-body {
+          height: 100%;
+        }
+      }
+    }
+  }
+  .product-list-header {
+    border-bottom: 1px solid #F0F2F6;
+  }
+</style>
