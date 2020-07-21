c05d6f38622ffeddee9d496bcc6b50367ec501e5
weiru <weiru@meituan.com>
2020-07-21 17:52:05 +0800

    feat: 去掉无用代码

--- src/views/components/configurable-form/instance/common-form/index.js
+++ src/views/components/configurable-form/instance/common-form/index.js
@@ -1,8 +1,6 @@
 import createBaseForm from '../base-form'
 import { getProduct, getContext } from './initData'
 import baseService from './service'
-// import CategoryAttrModule from '../../modules/category-attrs'
-// import { SPU_FELID } from '../../felid'
 import validate from './validate'
 
 const baseForm = createBaseForm(baseService)
@@ -19,9 +17,6 @@ export default ({
   }, {
     components,
     plugins,
-    // modules: {
-    //   [SPU_FELID.CATEGORY_ATTRS]: CategoryAttrModule
-    // },
     validate
   })
 }
--- src/views/components/configurable-form/instance/standard-audit/index.js
+++ src/views/components/configurable-form/instance/standard-audit/index.js
@@ -1,24 +1,18 @@
 import createBaseForm from '../base-form'
 import { getProduct, getContext } from './initData'
-// import CategoryAttrModule from '../../modules/category-attrs'
-// import { SPU_FELID } from '../../felid'
 import validate from './validate'
 
 const baseForm = createBaseForm()
 
-export default ({ service, components }) => {
+export default ({ components } = {}) => {
   const data = getProduct()
   const context = getContext()
   return baseForm({
     data,
     context
   }, {
-    service,
     components,
     plugins: [],
-    // modules: {
-    //   [SPU_FELID.CATEGORY_ATTRS]: CategoryAttrModule
-    // },
     validate
   })
 }
