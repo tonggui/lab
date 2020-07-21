6b451c885e6505b12dd11f0ae6f571c6fb421d98
danzewei <danzewei@meituan.com>
2020-07-21 20:13:39 +0800

    feat: sp-chang-info组件及choose-product位置调整

---
+++ src/views/components/configurable-form/components/sp-change-info/components/diff-item/item-default.vue
@@ -0,0 +1,12 @@
+<template>
+  <span>{{value}}</span>
+</template>
+
+<script>
+  export default {
+    name: 'NormalDiffItem',
+    props: {
+      value: [String, Number, Array, Object]
+    }
+  }
+</script>
