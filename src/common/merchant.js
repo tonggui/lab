93d0e82d304ddd6e733b54434b27e81dfdaed502
张良 <zhangliang15@meituan.com>
2020-08-14 17:07:24 +0800

    feat: 添加日志，定位问题

--- src/common/merchant.js
+++ src/common/merchant.js
@@ -10,15 +10,18 @@ export const ConfigKeys = {
 
 export const updateMerchantConfig = (key, value) => {
   commonMerchantConfig[key] = value
+  console.log('updateMerchantConfig', key, value)
   sessionStorage.setItem(getConfigKey(key), JSON.stringify({ value }))
   return commonMerchantConfig
 }
 
 export const getMerchantConfig = (key) => {
   if (key in commonMerchantConfig) {
+    console.log('getMerchantConfigCache', key in commonMerchantConfig, commonMerchantConfig)
     return commonMerchantConfig[key]
   } else {
     const cachedValue = sessionStorage.getItem(getConfigKey(key))
+    console.log('getMerchantConfig', key, cachedValue)
     if (cachedValue) {
       commonMerchantConfig[key] = cachedValue.value
       return cachedValue.value
