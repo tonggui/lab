40afa9b4b58eb72cdc133c9330d8444bf108d17f
张良 <zhangliang15@meituan.com>
2020-08-14 19:31:12 +0800

    feat: 使用新版本的批量传图功能的接口模式

--- src/views/progress-new/models/merchant/merchant-task-batch-upload-image.ts
+++ src/views/progress-new/models/merchant/merchant-task-batch-upload-image.ts
@@ -1,20 +1,28 @@
-import { MerchantProgressTask } from './merchant-progress-task'
-import { TaskAction, TaskActionType } from '@/views/progress-new/models/progress-task'
+// import { TaskAction, TaskActionType } from '@/views/progress-new/models/progress-task'
+import { NewMerchantProgressTask } from '@/views/progress-new/models/merchant/merchant-progress-task-new'
 
-class MerchantTaskBatchUploadImage extends MerchantProgressTask {
-  protected getExclusiveAction (): TaskAction[] {
-    const actionList: TaskAction[] = []
-    actionList.push({
-      text: '查看上传详情',
-      type: TaskActionType.Modal,
-      action: {
-        title: '查看上传详情',
-        modalType: 'DETAIL_UPLOAD_IMGS',
-        getData: () => this.fetchTaskDetail()
-      }
-    })
-    return actionList
+class MerchantTaskBatchUploadImage extends NewMerchantProgressTask {
+  get hasRelPoi (): boolean {
+    return false
   }
+
+  get sourceDisplayText (): string {
+    return '查看上传详情'
+  }
+
+  // protected getExclusiveAction (): TaskAction[] {
+  //   const actionList: TaskAction[] = []
+  //   actionList.push({
+  //     text: '查看上传详情',
+  //     type: TaskActionType.Modal,
+  //     action: {
+  //       title: '查看上传详情',
+  //       modalType: 'DETAIL_UPLOAD_IMGS',
+  //       getData: () => this.fetchTaskDetail()
+  //     }
+  //   })
+  //   return actionList
+  // }
 }
 
 export { MerchantTaskBatchUploadImage }
