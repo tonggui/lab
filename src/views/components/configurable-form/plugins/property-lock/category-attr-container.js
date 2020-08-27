159185ff57a3c400d82c2d2733a06bac4588c1c5
weiru <weiru@meituan.com>
2020-08-27 17:05:38 +0800

    feat: 添加注释

--- src/views/components/configurable-form/plugins/property-lock/category-attr-container.js
+++ src/views/components/configurable-form/plugins/property-lock/category-attr-container.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import { forwardComponent } from '@/common/vnode'
 import container from './container'
 
+// 给类目属性绑定的字段锁定container
+// 由于类目属性是 动态表单嵌套，并且属性动态生成
+// 因此，简单的通过attrContext => 中的 container 列表，进行 类目属性的包裹，从而实现额外功能
+// TODO 此方式不是很优雅，建议考虑更合适的方式
 export default (WrapperComponent) => Vue.extend({
   name: 'category-attr-property-lock-container',
   props: {
@@ -10,13 +14,17 @@ export default (WrapperComponent) => Vue.extend({
     attrContext: Object
   },
   computed: {
+    // 进行container的计算
     combineAttrContext () {
+      // 如果字段已锁定，说明不是字段锁定导致的锁定，忽略
       if (this.disabled) {
         return this.attrContext
       }
+      // 字段锁定更新
       const attrContext = this.attrContext || {}
       const attrList = this.attrList || []
       attrList.forEach(attr => {
+        // 必填的属性，才进行包裹
         if (attr.required) {
           const data = attrContext[attr.id] || {}
           attrContext[attr.id] = {
@@ -29,6 +37,7 @@ export default (WrapperComponent) => Vue.extend({
     }
   },
   render (h) {
+    // 传递 attrContext
     return forwardComponent(this, WrapperComponent, {
       props: {
         disabled: this.disabled,
--- src/views/components/configurable-form/plugins/suggest-tag-list/suggest-tag-list.js
+++ src/views/components/configurable-form/plugins/suggest-tag-list/suggest-tag-list.js
@@ -11,6 +11,7 @@ import TagListWithSuggest from '@/components/taglist/tag-list-with-suggest'
 import store from '@/store'
 import { getPathById } from '@/components/taglist/util'
 
+// 分类模版组件包装
 export default withCategoryTemplate(Vue.extend({
   name: 'suggest-tag-list-container',
   props: {
@@ -30,10 +31,22 @@ export default withCategoryTemplate(Vue.extend({
   },
   watch: {
     async categoryId () {
-      if (this.usedBusinessTemplate && this.categoryId) {
-        this.suggestList = await this.getSuggest(this.categoryId)
+      try {
+        // 门店使用了B端分类模版，并且商品有类目Id，获取推荐类目
+        if (this.usedBusinessTemplate && this.categoryId) {
+          this.suggestList = await this.getSuggest(this.categoryId)
+        }
+      } catch (err) {
+        console.error(err)
       }
     },
+    // url上tagId填充逻辑
+    /**
+     * 用户新建商品的时候：
+     * 如果门店使用了B端分类模版，则不进行url上的tagId 填充，通过推荐的方式引导用户
+     * 如果门店未使用B端分类模版，则填充url的tagId
+     * 此状态抽象到cmm的开关中了 POI_CREATE_PRODUCT_AUTO_FILL_TAG
+     */
     needFillTagByQuery: {
       handler () {
         this.fillTagByQuery()
@@ -43,12 +56,18 @@ export default withCategoryTemplate(Vue.extend({
   },
   computed: {
     ...mapModule({
+      // 门店是否有可以使用的B端分类模版
       haveBusinessTemplate: BUSINESS_CATEGORY_TEMPLATE,
+      // 配置的 一级店内分类 上限，进行分类模版引导
       tagMaxLimit: TAG_FIRST_LEVEL_LIMIT,
+      // 根据门店是否 使用B端分类模版，控制是否进行 url上tagId的填充
       needFillTagByQuery: POI_CREATE_PRODUCT_AUTO_FILL_TAG
     }),
+    // 分类模版引导文案
     needApplyWarning () {
+      // 门店有可以使用的B端分类模版 && 门店没有使用B端分类模版
       if (this.haveBusinessTemplate && !this.usedBusinessTemplate) {
+        // 门店的一级店内分类 数量 不在 [5, tagMaxLimit] 范围内，进行分类模版引导
         const tagCount = this.source.length
         const poor = tagCount < 5
         const rich = tagCount > this.tagMaxLimit
@@ -61,20 +80,29 @@ export default withCategoryTemplate(Vue.extend({
     }
   },
   async mounted () {
-    this.unsubscribeAction = store.subscribeAction(action => {
-      if (action.type === 'categoryTemplate/successBroadcast') {
-        this.getTagList()
-      }
-    })
-    await this.getTagList()
-    this.fillTagByQuery()
+    try {
+      // 编辑页 进行分类模版的使用时，需要监听，分类模版成功信号，成功之后，进行 店内分类更新
+      // 避免用户填写信息丢失，只更新 店内分类
+      this.unsubscribeAction = store.subscribeAction(action => {
+        if (action.type === 'categoryTemplate/successBroadcast') {
+          this.getTagList()
+        }
+      })
+      // 挂载之后，获取店内分类
+      await this.getTagList()
+      this.fillTagByQuery()
+    } catch (err) {
+      console.error(err)
+    }
   },
   beforeDestroy () {
+    // 卸载之前，取消 分类模版成功的监听
     if (this.unsubscribeAction) {
       this.unsubscribeAction()
     }
   },
   methods: {
+    // 获取店内分类列表
     async getTagList () {
       try {
         this.source = await this.getSource()
@@ -83,6 +111,7 @@ export default withCategoryTemplate(Vue.extend({
         this.source = []
       }
     },
+    // 打开分类模版
     showCategoryTemplate () {
       this.$emit('show-category-template')
     },
@@ -90,6 +119,8 @@ export default withCategoryTemplate(Vue.extend({
     fillTagByQuery () {
       const tagId = +this.$route.query.tagId
       const empty = !this.value || !this.value.length
+      // 新建场景下，url上有tagId，支持填充，tagId字段未填充值
+      // 根据tagList，获取url上tagId对应的tag信息，获取到之后，进行填充
       if (!this.spuId && tagId && this.needFillTagByQuery && empty) {
         const path = getPathById(tagId, this.source)
         if (path && path.length) {
@@ -99,6 +130,7 @@ export default withCategoryTemplate(Vue.extend({
     }
   },
   render (h) {
+    // 门店没有可以使用的B端分类模版，降级成 tagList组件（不带推荐的店内分类组件）
     if (!this.haveBusinessTemplate) {
       return h(TagList, {
         props: {
@@ -108,6 +140,7 @@ export default withCategoryTemplate(Vue.extend({
         on: this.$listeners
       })
     }
+    // 渲染带推荐的店内分类 组件
     return h(TagListWithSuggest, {
       props: {
         value: this.value,
