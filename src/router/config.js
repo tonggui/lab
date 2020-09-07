6d788a1556fe91ace9af23d317eb39851072d038
danzewei <danzewei@meituan.com>
2020-09-07 10:44:37 +0800

    feat: 文件调整

--- src/router/config.js
+++ src/router/config.js
@@ -407,14 +407,6 @@ const routeList = [
       import(
         /* webpackChunkName: "errorPage" */ '../views/error/index.vue'
       )
-  },
-  {
-    name: 'test',
-    path: '/test',
-    component: () =>
-      import(
-        /* webpackChunkName: "errorPage" */ '../views/assemble-page/index.js'
-        )
   }
 ]
 
--- src/views/assemble-page/utils.js
+++
@@ -1,3 +0,0 @@
-export function getFuncName (func) {
-  return this.name || this.toString().match(/function\s*([^(]*)\(/)[1]
-}
