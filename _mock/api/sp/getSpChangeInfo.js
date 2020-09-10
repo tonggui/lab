c1bb31ba026a909e8085dc2b20373563b4b0a32c
danzewei <danzewei@meituan.com>
2020-09-10 14:48:08 +0800

    feat: 标品类目属性问题及标品更新弹窗问题修复

--- _mock/api/sp/getSpChangeInfo.js
+++ _mock/api/sp/getSpChangeInfo.js
@@ -2,104 +2,104 @@
  * @url /reuse/sc/product/retail/v2/r/getProductChangeInfo
  */
 module.exports = function () {
-  return {
-    data: {
-      basicInfoListSize: 1,
-      basicInfoListIterator: [
-        {
-          setOldValue: true,
-          setNewValue: true,
-          newValue: 'http://p0.meituan.net/xianfu/cedbd0cbad0ea482c5307f61fdf547eb146003.jpg,http://p0.meituan.net/xianfu/026b1ae6201a50bf5a4b523dcb6bab98155350.jpg,http://p0.meituan.net/xianfu/83b58b2c01e2c7b1564187dde31cd05879888.jpg',
-          oldValue: '',
-          field: '10',
-          setField: true
-        }
-      ],
-      setBasicInfoList: true,
-      categoryInfoListSize: 4,
-      categoryInfoListIterator: [
-        {
-          setOldValue: true,
-          setNewValue: true,
-          newValue: '温馨提示：1.OTC药品请按药品说明书或在药师指导下购买和使用。2.医疗器械请仔细阅读产品说明书或者在医务人员的指导下购买和使用，禁忌内容或者注意事项详见说明书。3.保健食品不能代替药物。4.由于厂家不定期的更换产品包装，如遇新包装上市可能存在更新滞后，请以收到的实货包装为准！5.药品属于特殊商品，除药品质量原因外，药品一经售出，不得退换。',
-          oldValue: '1.OTC药品请按药品说明书或在药师指导下购买和使用。\r\n2.医疗器械请仔细阅读产品说明书或者在医务人员的指导下购买和使用，禁忌内容或者注意事项详见说明书。\r\n3.保健品不能代替药物。\r\n4.由于厂家不定期的更换产品包装，如遇新包装上市可能存在更新滞后，请以收到的实货包装为准！\r\n5.药品属于特殊商品，除药品质量原因外，药品一经售出，不得退换。',
-          field: '1200000015',
-          setField: true
-        },
-        {
-          setOldValue: true,
-          setNewValue: true,
-          newValue: '',
-          oldValue: '玻璃瓶。',
-          field: '1200000009',
-          setField: true
-        },
-        {
-          setOldValue: true,
-          setNewValue: true,
-          newValue: '',
-          oldValue: '鲜竹沥、鱼腥草、生半夏、生姜、枇杷叶、桔梗、薄荷素油。辅料为蔗糖和防腐剂（苯甲酸钠）。',
-          field: '1200000012',
-          setField: true
-        },
-        {
-          setOldValue: true,
-          setNewValue: true,
-          newValue: '鲜竹沥、鱼腥草、生半夏、生姜、枇杷叶、桔梗、薄荷素油。辅料为蔗糖和防腐剂（苯甲酸钠）。',
-          oldValue: '',
-          field: '1200000311',
-          setField: true
-        }
-      ],
-      setCategoryInfoList: true,
-      basicInfoList: [
-        {
-          setOldValue: true,
-          setNewValue: true,
-          newValue: 'http://p0.meituan.net/xianfu/cedbd0cbad0ea482c5307f61fdf547eb146003.jpg,http://p0.meituan.net/xianfu/026b1ae6201a50bf5a4b523dcb6bab98155350.jpg,http://p0.meituan.net/xianfu/83b58b2c01e2c7b1564187dde31cd05879888.jpg',
-          oldValue: '',
-          field: '10',
-          setField: true
-        }
-      ],
-      categoryInfoList: [
-        {
-          setOldValue: true,
-          setNewValue: true,
-          newValue: '温馨提示：1.OTC药品请按药品说明书或在药师指导下购买和使用。2.医疗器械请仔细阅读产品说明书或者在医务人员的指导下购买和使用，禁忌内容或者注意事项详见说明书。3.保健食品不能代替药物。4.由于厂家不定期的更换产品包装，如遇新包装上市可能存在更新滞后，请以收到的实货包装为准！5.药品属于特殊商品，除药品质量原因外，药品一经售出，不得退换。',
-          oldValue: '1.OTC药品请按药品说明书或在药师指导下购买和使用。\r\n2.医疗器械请仔细阅读产品说明书或者在医务人员的指导下购买和使用，禁忌内容或者注意事项详见说明书。\r\n3.保健品不能代替药物。\r\n4.由于厂家不定期的更换产品包装，如遇新包装上市可能存在更新滞后，请以收到的实货包装为准！\r\n5.药品属于特殊商品，除药品质量原因外，药品一经售出，不得退换。',
-          field: '1200000015',
-          setField: true
-        },
-        {
-          setOldValue: true,
-          setNewValue: true,
-          newValue: '',
-          oldValue: '玻璃瓶。',
-          field: '1200000009',
-          setField: true
-        },
-        {
-          setOldValue: true,
-          setNewValue: true,
-          newValue: '',
-          oldValue: '鲜竹沥、鱼腥草、生半夏、生姜、枇杷叶、桔梗、薄荷素油。辅料为蔗糖和防腐剂（苯甲酸钠）。',
-          field: '1200000012',
-          setField: true
-        },
-        {
-          setOldValue: true,
-          setNewValue: true,
-          newValue: '鲜竹沥、鱼腥草、生半夏、生姜、枇杷叶、桔梗、薄荷素油。辅料为蔗糖和防腐剂（苯甲酸钠）。',
-          oldValue: '',
-          field: '1200000311',
-          setField: true
-        }
-      ]
-    },
-    code: 0,
-    msg: '成功'
-  };
+  // return {
+  //   data: {
+  //     basicInfoListSize: 1,
+  //     basicInfoListIterator: [
+  //       {
+  //         setOldValue: true,
+  //         setNewValue: true,
+  //         newValue: 'http://p0.meituan.net/xianfu/cedbd0cbad0ea482c5307f61fdf547eb146003.jpg,http://p0.meituan.net/xianfu/026b1ae6201a50bf5a4b523dcb6bab98155350.jpg,http://p0.meituan.net/xianfu/83b58b2c01e2c7b1564187dde31cd05879888.jpg',
+  //         oldValue: '',
+  //         field: '10',
+  //         setField: true
+  //       }
+  //     ],
+  //     setBasicInfoList: true,
+  //     categoryInfoListSize: 4,
+  //     categoryInfoListIterator: [
+  //       {
+  //         setOldValue: true,
+  //         setNewValue: true,
+  //         newValue: '温馨提示：1.OTC药品请按药品说明书或在药师指导下购买和使用。2.医疗器械请仔细阅读产品说明书或者在医务人员的指导下购买和使用，禁忌内容或者注意事项详见说明书。3.保健食品不能代替药物。4.由于厂家不定期的更换产品包装，如遇新包装上市可能存在更新滞后，请以收到的实货包装为准！5.药品属于特殊商品，除药品质量原因外，药品一经售出，不得退换。',
+  //         oldValue: '1.OTC药品请按药品说明书或在药师指导下购买和使用。\r\n2.医疗器械请仔细阅读产品说明书或者在医务人员的指导下购买和使用，禁忌内容或者注意事项详见说明书。\r\n3.保健品不能代替药物。\r\n4.由于厂家不定期的更换产品包装，如遇新包装上市可能存在更新滞后，请以收到的实货包装为准！\r\n5.药品属于特殊商品，除药品质量原因外，药品一经售出，不得退换。',
+  //         field: '1200000015',
+  //         setField: true
+  //       },
+  //       {
+  //         setOldValue: true,
+  //         setNewValue: true,
+  //         newValue: '',
+  //         oldValue: '玻璃瓶。',
+  //         field: '1200000009',
+  //         setField: true
+  //       },
+  //       {
+  //         setOldValue: true,
+  //         setNewValue: true,
+  //         newValue: '',
+  //         oldValue: '鲜竹沥、鱼腥草、生半夏、生姜、枇杷叶、桔梗、薄荷素油。辅料为蔗糖和防腐剂（苯甲酸钠）。',
+  //         field: '1200000012',
+  //         setField: true
+  //       },
+  //       {
+  //         setOldValue: true,
+  //         setNewValue: true,
+  //         newValue: '鲜竹沥、鱼腥草、生半夏、生姜、枇杷叶、桔梗、薄荷素油。辅料为蔗糖和防腐剂（苯甲酸钠）。',
+  //         oldValue: '',
+  //         field: '1200000311',
+  //         setField: true
+  //       }
+  //     ],
+  //     setCategoryInfoList: true,
+  //     basicInfoList: [
+  //       {
+  //         setOldValue: true,
+  //         setNewValue: true,
+  //         newValue: 'http://p0.meituan.net/xianfu/cedbd0cbad0ea482c5307f61fdf547eb146003.jpg,http://p0.meituan.net/xianfu/026b1ae6201a50bf5a4b523dcb6bab98155350.jpg,http://p0.meituan.net/xianfu/83b58b2c01e2c7b1564187dde31cd05879888.jpg',
+  //         oldValue: '',
+  //         field: '10',
+  //         setField: true
+  //       }
+  //     ],
+  //     categoryInfoList: [
+  //       {
+  //         setOldValue: true,
+  //         setNewValue: true,
+  //         newValue: '温馨提示：1.OTC药品请按药品说明书或在药师指导下购买和使用。2.医疗器械请仔细阅读产品说明书或者在医务人员的指导下购买和使用，禁忌内容或者注意事项详见说明书。3.保健食品不能代替药物。4.由于厂家不定期的更换产品包装，如遇新包装上市可能存在更新滞后，请以收到的实货包装为准！5.药品属于特殊商品，除药品质量原因外，药品一经售出，不得退换。',
+  //         oldValue: '1.OTC药品请按药品说明书或在药师指导下购买和使用。\r\n2.医疗器械请仔细阅读产品说明书或者在医务人员的指导下购买和使用，禁忌内容或者注意事项详见说明书。\r\n3.保健品不能代替药物。\r\n4.由于厂家不定期的更换产品包装，如遇新包装上市可能存在更新滞后，请以收到的实货包装为准！\r\n5.药品属于特殊商品，除药品质量原因外，药品一经售出，不得退换。',
+  //         field: '1200000015',
+  //         setField: true
+  //       },
+  //       {
+  //         setOldValue: true,
+  //         setNewValue: true,
+  //         newValue: '',
+  //         oldValue: '玻璃瓶。',
+  //         field: '1200000009',
+  //         setField: true
+  //       },
+  //       {
+  //         setOldValue: true,
+  //         setNewValue: true,
+  //         newValue: '',
+  //         oldValue: '鲜竹沥、鱼腥草、生半夏、生姜、枇杷叶、桔梗、薄荷素油。辅料为蔗糖和防腐剂（苯甲酸钠）。',
+  //         field: '1200000012',
+  //         setField: true
+  //       },
+  //       {
+  //         setOldValue: true,
+  //         setNewValue: true,
+  //         newValue: '鲜竹沥、鱼腥草、生半夏、生姜、枇杷叶、桔梗、薄荷素油。辅料为蔗糖和防腐剂（苯甲酸钠）。',
+  //         oldValue: '',
+  //         field: '1200000311',
+  //         setField: true
+  //       }
+  //     ]
+  //   },
+  //   code: 0,
+  //   msg: '成功'
+  // };
   //   data: {
   //     basicInfoList: [
   //       {
@@ -128,4 +128,102 @@ module.exports = function () {
   //   code: 0,
   //   msg: ""
   // };
+  return {
+    data: {
+      basicInfoListSize: 1,
+      basicInfoListIterator: [
+        {
+          setOldValue: true,
+          setNewValue: true,
+          newValue: '18.0',
+          oldValue: '0.0',
+          field: '12',
+          setField: true
+        }
+      ],
+      setBasicInfoList: true,
+      categoryInfoListSize: 4,
+      categoryInfoListIterator: [
+        {
+          setOldValue: true,
+          setNewValue: true,
+          newValue: '1',
+          oldValue: '国产',
+          field: '1200000085',
+          setField: true
+        },
+        {
+          setOldValue: true,
+          setNewValue: true,
+          newValue: '12',
+          oldValue: 'OTC',
+          field: '1200000081',
+          setField: true
+        },
+        {
+          setOldValue: true,
+          setNewValue: true,
+          newValue: '',
+          oldValue: '0',
+          field: '020109',
+          setField: true
+        },
+        {
+          setOldValue: true,
+          setNewValue: true,
+          newValue: '',
+          oldValue: '0',
+          field: '020105',
+          setField: true
+        }
+      ],
+      setCategoryInfoList: true,
+      basicInfoList: [
+        {
+          setOldValue: true,
+          setNewValue: true,
+          newValue: '18.0',
+          oldValue: '0.0',
+          field: '12',
+          setField: true
+        }
+      ],
+      categoryInfoList: [
+        // {
+        //   setOldValue: true,
+        //   setNewValue: true,
+        //   newValue: '1',
+        //   oldValue: '国产',
+        //   field: '1200000085',
+        //   setField: true
+        // },
+        {
+          setOldValue: true,
+          setNewValue: true,
+          newValue: 1300000152,
+          oldValue: 'OTC',
+          field: '1200000081',
+          setField: true
+        },
+        {
+          setOldValue: true,
+          setNewValue: true,
+          newValue: '',
+          oldValue: '0',
+          field: '020109',
+          setField: true
+        },
+        {
+          setOldValue: true,
+          setNewValue: true,
+          newValue: '',
+          oldValue: '0',
+          field: '020105',
+          setField: true
+        }
+      ]
+    },
+    code: 0,
+    msg: '成功'
+  };
 };
