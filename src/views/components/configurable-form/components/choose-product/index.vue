d93a08376118854fb004c31c19a8eae5684e0ddd
张良 <zhangliang15@meituan.com>
2020-09-24 10:24:11 +0800

    feat: 完成顶部UPC选择组件（缺样式 + 弹窗选择逻辑 + 清空输入框）

--- src/views/components/configurable-form/components/choose-product/index.vue
+++ src/views/components/configurable-form/components/choose-product/index.vue
@@ -1,56 +1,75 @@
 <template>
   <div class="choose-product">
-    <p>推荐从商品库中选择商品，快捷录入信息</p>
-    <CustomSearchSelector
-      ref="custom-search"
-      class="search-selector"
-      :width="274"
-      :loading="loading"
-      :error="error"
-      :dataSource="dataSource"
-      :total="pagination.total"
-      :value="val"
-      :disabled="disabled"
-      placeholder="输入条形码/商品名称/品牌名"
-      @on-input-change="handleInputChange"
-      @on-input-blur="error = null"
-      @on-reach-bottom="handleReachBottom"
-      @on-input-enter="handleInputEnter"
-    >
-      <template v-slot:list-item="{ data, index, keyword }">
-        <li
-          is="ProductInfo"
-          :class="{'search-selector-list-item': true, 'gray': isDisabled(data)}"
-          :keyword="keyword"
-          :key="data.id + index"
-          :product="data"
-          @click.native="handleClickItem(data)"
-        />
-      </template>
-    </CustomSearchSelector>
-    <Button :disabled="disabled" type="primary" @click="$emit('showSpListModal')" v-if="supportProductLibrary">从商品库选择</Button>
-    <AuditFieldTip :contents="auditTips" />
-    <a :class="{ 'delete': true, 'disabled': disabled }" @click="handleDeleteQuickSelect" v-if="val">删除快捷录入</a>
+    <div>{{confirmed ? '已使用商品库信息，商品无需审核' : '直接使用平台商品库信息创建，商品无需审核。请先找一找您要创建的商品吧～'}}</div>
+    <div class="choose-product-content">
+      <CustomSearchSelector
+        v-if="!confirmed"
+        ref="custom-search"
+        class="search-selector"
+        :width="274"
+        :loading="loading"
+        :error="error"
+        :dataSource="dataSource"
+        :total="pagination.total"
+        :value="val"
+        :disabled="disabled"
+        :inputAttrs="{ search: true, 'enter-button': true }"
+        placeholder="输入商品条形/名称/品牌名查找"
+        @on-input-change="handleInputChange"
+        @on-input-blur="handleSelectorBlur"
+        @on-input-focus="handleSelectorFocus"
+        @on-reach-bottom="handleReachBottom"
+        @on-input-enter="handleInputEnter"
+        @on-select-item="handleClickItem"
+      >
+        <template v-slot:list-item="{ data, index, keyword }">
+          <component
+            :is="`ProductInfo`"
+            :class="{'search-selector-list-item': true }"
+            :keyword="keyword"
+            :key="data.id + index"
+            :product="data"
+          />
+        </template>
+        <template slot="loading">
+          <div class="loading-container">
+            <FlashLoading size="mini" />
+            <div class="loading-tips">从商品库查找中</div>
+          </div>
+        </template>
+        <template slot="empty">
+          暂未找到，请手动创建
+        </template>
+      </CustomSearchSelector>
+      <Tag
+        v-else
+        closable
+        @on-close="handleReselectEvent"
+      >{{selectedItem.name}}</Tag>
+      <Button
+        class="primary-style-button"
+        :disabled="disabled"
+        type="text"
+        @click="$emit('showSpListModal')"
+        v-if="supportProductLibrary">通过目录查找 ></Button>
+      <AuditFieldTip :contents="auditTips" />
+    </div>
   </div>
 </template>
 
 <script>
+  import FlashLoading from '@/components/loading/flash-loading'
   import CustomSearchSelector from '@/components/custom-search-selector'
   import ProductInfo from './product-info'
-  import { debounce } from 'lodash'
+  import { debounce, noop } from 'lodash'
   import { fetchGetSpList } from '@/data/repos/standardProduct'
-  // import { QUALIFICATION_STATUS } from '@/data/enums/product'
-  // import qualificationModal from '@/components/qualification-modal'
   import AuditFieldTip from '@/views/components/product-form/components/audit-field-tip'
   import { QUALIFICATION_STATUS } from '@/data/enums/product'
-  // import { poiId } from '@/common/constants'
-  // import Icon from '@/components/icon/icon'
-  // import lx from '@/common/lx/lxReport'
 
-  // const UPC_NOT_FOUND_FAIL = '条码暂未收录，请直接录入商品信息'
   export default {
     name: 'ChooseProduct',
     components: {
+      FlashLoading,
       AuditFieldTip,
       CustomSearchSelector,
       ProductInfo
@@ -66,6 +85,11 @@
         val: this.value,
         error: null,
         loading: false,
+        selectedItem: null,
+        confirmed: false,
+        // 联合确认当前失焦状态是否需要触发回滚逻辑
+        modalVisible: false,
+        confirmVisible: false,
         dataSource: [],
         pagination: {
           current: 1,
@@ -99,6 +123,7 @@
           this.loading = false
           const { pagination, list } = res
           this.pagination = pagination
+          list.forEach(item => (item.disabled = this.isDisabled(item)))
           this.dataSource.push(...list)
         }).catch(err => {
           this.error = err.message
@@ -136,13 +161,41 @@
         //     this.loading = false
         //   })
       },
-      handleClickItem (item) {
-        if (this.isDisabled(item)) return
-        this.$emit('on-select-product', item)
-        // 选中商品时重新获取商品信息
-        this.dataSource = []
-        this.getSpList(item.name)
-        this.$refs['custom-search'].hide()
+      async handleClickItem (item, cb = noop) {
+        if (this.isDisabled(item)) {
+          cb(new Error('禁用选项，禁止选中！'))
+          return
+        }
+
+        const isAccepted = await new Promise(resolve => {
+          if (!this.selectedItem) {
+            resolve(true)
+            return
+          }
+          this.confirmVisible = true
+          this.$Modal.confirm({
+            title: '确定选择此商品',
+            content: '选择此商品后，已填写的商品信息将被覆盖。是否选择此商品？',
+            onOk: () => resolve(true),
+            onCancel: () => resolve(false)
+          })
+        })
+        this.confirmVisible = false
+
+        if (isAccepted) {
+          this.$emit('on-select-product', item)
+          this.selectedItem = item
+          this.confirmed = true
+          // 选中商品时重新获取商品信息
+          this.dataSource = []
+          this.getSpList(item.name)
+          this.$refs['custom-search'].hide()
+          this.$Message.success('信息填充成功，请继续完善')
+        } else {
+          // 取消后，重新设置为选中状态。保持选中的场景
+          this.$refs['custom-search'].setFocusState()
+        }
+        cb(isAccepted ? null : new Error())
       },
       handleChange (val) {
         this.val = val
@@ -150,67 +203,6 @@
         this.$emit('input', this.val)
         this.$emit('on-change', this.val)
       },
-      // triggerSearch () {
-      //   const upcCode = this.val
-      //   // 如果和缓存的最后一次查询结果相同，避免请求
-      //   if (this.lastSearchUpc === upcCode) return
-      //   this.lastSearchUpc = upcCode
-      //   // 如果为空，避免请求
-      //   if (!upcCode) return
-      //   return fetchGetSpInfoByUpc(upcCode, poiId)
-      //     .then(product => {
-      //       this.error = null
-      //       this.triggerSelectProduct(product)
-      //     })
-      //     .catch(err => {
-      //       let error = null
-      //       if (err.code === 6000) {
-      //         error = UPC_NOT_FOUND_FAIL
-      //       } else if (err.code === QUALIFICATION_STATUS.NO || err.code === QUALIFICATION_STATUS.EXP) {
-      //         qualificationModal(err.message)
-      //       } else if (err.code === 2) {
-      //         error = err.message
-      //         // 存在返回的数据
-      //         // TODO 现在只存在后台类目信息
-      //         if (err.data && err.data.category) {
-      //           const category = err.data.category
-      //           this.$emit('on-update-category', {
-      //             id: category.id,
-      //             idPath: category.idPath,
-      //             name: category.name,
-      //             namePath: category.namePath,
-      //             isLeaf: category.isLeaf,
-      //             level: category.level
-      //           })
-      //         }
-      //       } else {
-      //         if (err.code === QUALIFICATION_STATUS.NOT_ALLOWED) {
-      //           // 不可售卖商品提示埋点
-      //           lx.mv({
-      //             bid: 'b_shangou_online_e_pz7m7ncm_mv',
-      //             val: { type: 1 } // 超出经营范围
-      //           })
-      //         }
-      //         error = err.message
-      //       }
-      //       // 清空选择状态，支持下次查询
-      //       this.lastSearchUpc = ''
-      //       this.error = error
-      //       this.$emit('upcSugFailed', upcCode)
-      //     })
-      // },
-      // triggerSelectProduct (product) {
-      //   this.$emit('on-select-product', product)
-      // },
-      // 记录foucs之前的value，避免未修改value导致的第一次默认查询，容易修改类目属性的信息
-      // handleFocusEvent () {
-      //   this.preValue = this.val
-      // },
-      // handleBlurEvent () {
-      //   if (this.val !== this.preValue) {
-      //     this.triggerSearch()
-      //   }
-      // },
       handleDeleteQuickSelect () {
         this.$Modal.open({
           width: '362px',
@@ -223,7 +215,27 @@
             this.$emit('delete-all-data')
           }
         })
+      },
+      handleSelectorBlur () {
+        this.error = null
+        if ([this.modalVisible, this.confirmVisible].some(any => any)) {
+          return
+        }
+        if (this.selectedItem) {
+          this.confirmed = true
+        }
+      },
+      handleReselectEvent () {
+        this.confirmed = false
+        this.$nextTick(() => {
+          if (this.$refs['custom-search']) {
+            this.$refs['custom-search'].setFocusState()
+          }
+        })
       }
+    },
+    created () {
+      this._clearFocusStateHandler = 0
     }
   }
 </script>
@@ -231,60 +243,61 @@
 <style scoped lang="less">
   @import '~@/styles/common.less';
   .choose-product {
-    display: flex;
-    align-items: center;
-    .search-selector {
-      margin-left: 20px;
-      margin-right: 10px;
-      &-list-item {
-        min-height: 82px;
-        border-bottom: 1px solid #F2F2F7;
-        padding: 4px 10px 0;
-        &:hover {
-          background: #F3F4F6;
-          cursor: pointer;
-        }
-        &.gray {
-          width: 100%;
+    padding: 0 10px;
+    .header-tip {
+      width: 100%;
+    }
+    .choose-product-content {
+      margin-top: 8px;
+      display: flex;
+      align-items: center;
+      .search-selector {
+        margin-right: 10px;
+        &-list-item {
           min-height: 82px;
-          position: relative;
-          &::before {
-            content: '';
-            position: absolute;
-            display: inline-block;
-            cursor: not-allowed;
-            left: 0;
-            top: 0;
-            width: 100%;
-            height: 100%;
-            min-height: 82px;
-            background: #fff;
-            z-index: 1;
-            opacity: 0.5;
+          border-bottom: 1px solid #F2F2F7;
+          padding: 4px 10px 0;
+        }
+
+        .loading-container {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+
+          .loading-tips {
+            font-size: 14px;
+            color: #999999;
+            margin-left: 10px;
           }
         }
       }
-    }
-    /deep/ .boo-tabs-bar {
-      margin-bottom: 20px;
-    }
-    /deep/ .boo-tabs {
-      overflow: visible;
-      .boo-tooltip-inner {
-        max-width: none;
-        white-space: normal;
+      /deep/ .boo-tabs-bar {
+        margin-bottom: 20px;
       }
-    }
-    .delete {
-      font-family: PingFangSC-Regular;
-      font-size: 14px;
-      color: #3F4156;
-      letter-spacing: 0;
-      text-decoration: underline;
-      margin-left: 16px;
-      &.disabled {
-        pointer-events: none;
-        color: #D9D9D9;
+      /deep/ .boo-tabs {
+        overflow: visible;
+        .boo-tooltip-inner {
+          max-width: none;
+          white-space: normal;
+        }
+      }
+      .delete {
+        font-family: PingFangSC-Regular;
+        font-size: 14px;
+        color: #3F4156;
+        letter-spacing: 0;
+        text-decoration: underline;
+        margin-left: 16px;
+        &.disabled {
+          pointer-events: none;
+          color: #D9D9D9;
+        }
+      }
+
+      /deep/ .boo-tag {
+        font-size: @font-size-base;
+        height: 36px;
+        line-height: 36px;
       }
     }
   }
@@ -310,5 +323,9 @@
   .boo-input-icon-scan {
     font-size: @font-size-base;
     height: 36px;
+  }
+
+  .primary-style-button {
+    color: @highlight-color;
   }
 </style>
