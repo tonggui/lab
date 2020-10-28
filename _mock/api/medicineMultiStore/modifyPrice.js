b99d6c7d392a26387a2c0d6b687f8016d00e8c69
袁帅 <yuanshuai15@meituan.com>
2020-10-28 17:34:36 +0800

    feat: 调价

---
+++ _mock/api/medicineMultiStore/modifyPrice.js
@@ -0,0 +1,9 @@
+/**
+ * @url reuse/sc/product/medicine/modify/price/save
+*/
+module.exports = function (req) {
+    return {
+      code: 0,
+      msg: ''
+    }
+  }
---
+++ _mock/api/medicineMultiStore/modifyStock.js
@@ -0,0 +1,9 @@
+/**
+ * @url reuse/sc/product/medicine/modify/stock/save
+*/
+module.exports = function (req) {
+    return {
+      code: 0,
+      msg: ''
+    }
+  }
