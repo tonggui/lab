38b294a56801eef4b5af03b782e5e25aae843d78
baohongmei <baohongmei@meituan.com>
2020-10-30 17:12:00 +0800

    feat: 【多门店管理】-全选本页&全部～处理

--- _mock/api/medicineMultiStore/getList.js
+++ _mock/api/medicineMultiStore/getList.js
@@ -52,7 +52,49 @@
                 //   spec: '150ml'
                 // }
               ],
-            }
+            },
+            {
+              wmPoiId: 122,
+              wmPoiName: '门店名称',
+              spuId: 23,
+              sourceFoodCode: '123123',
+              'name|1':["酷儿维生素C+钙香橙汁450ml酷儿维生素C+钙香橙汁450ml酷儿维生素C+钙香橙汁450ml",
+                        "可口可乐"],
+              upcCode: '123123123123123',
+              picture: 'http://p0.meituan.net/xianfu/6d593a6582f4a31e1b51c670634c4a32131072.jpg',
+              tagName: '商家分类名称',
+              categoryName1:'一级后台分类名称',
+              categoryName2: '2级后台分类名称',
+              categoryName3: '3级后台分类名称',
+              medicineType: 0,
+              "medicineTypeName|1": ['处方药',"OTC","通用药","非药品"],
+              sellStatus: 0,
+              sellStatusName: '上架',
+              price: 233,
+              stock: 40,
+              wmProductSkus: [{
+                id: 0,
+                upcCode: 123,
+                price: 10,
+                stock: 0,
+                spec: '450ml',
+                weight: '克(g)'
+              }, 
+              // {
+              //   id: 1,
+              //   upcCode: 123,
+              //   price: 12345.12,
+              //   stock: 0,
+              //   spec: '450ml'
+              // }, {
+              //   id: 2,
+              //   upcCode: 123,
+              //   price: 12345.12,
+              //   stock: 0,
+              //   spec: '150ml'
+              // }
+            ],
+          }
         ]
       }
     }
