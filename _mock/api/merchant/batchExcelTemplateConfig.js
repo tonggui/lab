bc3a4ad12738867a53ee5b64305a09ab395e3c5b
张良 <zhangliang15@meituan.com>
2020-12-08 12:15:41 +0800

    feat: 修复商家商品中心场景下，批量修改无法下载右上角类目表的问题

--- _mock/api/merchant/batchExcelTemplateConfig.js
+++ _mock/api/merchant/batchExcelTemplateConfig.js
@@ -2,32 +2,49 @@
  * @url /reuse/sc/product/hqcc/batch/r/config
  */
 module.exports = {
-  code: 0,
-  msg: '',
-  data: {
-    createWithEan: {
-      url: "//s3plus.sankuai.com/v1/mss_cf11584f34294f138b03f87308a62093/product-batch-template/%E6%9D%A1%E7%A0%81%E5%8C%B9%E9%85%8D%E5%95%86%E5%93%81%E5%BA%93%E5%95%86%E5%93%81%E8%A1%A8%E6%A0%BC.xls",
-      meta: {
-        lastModifyTime: 1597313038075,
-        version: "1.1.2",
-        fields: "upcCode,price,stock,tagName,subTagName,sourceFoodCode,locatorCode,minSaleCount,sellStatus,description,appFoodCode"
-      }
+  'data': {
+    'retailCategoryTpl': {
+      'meta': { 'lastModifyTime': 1603382400000, 'version': '1.0.0' },
+      'lastModifyTime': -1,
+      'version': '1.0.0',
+      'url': '//s3plus.sankuai.com/v1/mss_cf11584f34294f138b03f87308a62093/product-batch-template/%E5%95%86%E5%93%81%E7%B1%BB%E7%9B%AEid%E8%A1%A8%EF%BC%8820201023%EF%BC%89.xls',
+      'fields': []
     },
-    createWithoutEan: {
-      url: "//s3plus.sankuai.com/v1/mss_cf11584f34294f138b03f87308a62093/product-batch-template/%E8%87%AA%E8%A1%8C%E5%88%9B%E5%BB%BA%E5%95%86%E5%93%81%E8%A1%A8%E6%A0%BC%EF%BC%8820200608%EF%BC%89.xls",
-      meta: {
-        lastModifyTime: 1597313038075,
-        version: "1.1.5",
-        fields: "categoryId,title,originName,brandName,spec,price,stock,weight,weightUnit,upcCode,sourceFoodCode,tagName,subTagName,locatorCode,minSaleCount,sellStatus,description,appFoodCode"
-      }
+    'hqccCreateWithCustom': {
+      'meta': {
+        'lastModifyTime': 1603382400000,
+        'fields': 'categoryId,title,originName,brandName,spec,price,stock,weight,weightUnit,upcCode,sourceFoodCode,tagName,subTagName,locatorCode,minSaleCount,sellStatus,description,appFoodCode',
+        'version': '1.1.6'
+      },
+      'lastModifyTime': -1,
+      'version': '1.1.6',
+      'url': '//s3plus.sankuai.com/v1/mss_cf11584f34294f138b03f87308a62093/hqcc-batch-template/%E8%87%AA%E8%A1%8C%E5%88%9B%E5%BB%BA%E5%95%86%E5%93%81%E8%A1%A8%E6%A0%BC%EF%BC%8820201023%EF%BC%89.xls',
+      'fields': ['categoryId', 'title', 'originName', 'brandName', 'spec', 'price', 'stock', 'weight', 'weightUnit', 'upcCode', 'sourceFoodCode', 'tagName', 'subTagName', 'locatorCode', 'minSaleCount', 'sellStatus', 'description', 'appFoodCode']
     },
-    updateTpl: {
-      url: "//s3plus.sankuai.com/v1/mss_cf11584f34294f138b03f87308a62093/product-batch-template/%E6%89%B9%E9%87%8F%E4%BF%AE%E6%94%B9%E6%A8%A1%E7%89%88%EF%BC%8820200608%EF%BC%89.xls",
-      meta: {
-        lastModifyTime: 1597313038075,
-        version: "1.1.5",
-        fields: "matchTitle,matchUpc,matchSourceFoodCode,matchSkuId,categoryId,title,originName,brandName,spec,price,stock,weight,weightUnit,upcCode,sourceFoodCode,tagName,subTagName,locatorCode,minSaleCount,sellStatus,description,appFoodCode"
-      }
+    'hqccCreateWithUpc': {
+      'meta': {
+        's_col': 1,
+        'lastModifyTime': 1557300000000,
+        'field_size': 11,
+        'fields': 'upcCode,price,stock,tagName,subTagName,sourceFoodCode,locatorCode,minSaleCount,sellStatus,description,appFoodCode',
+        'version': '1.1.6',
+        's_row': 4
+      },
+      'lastModifyTime': -1,
+      'version': '1.1.6',
+      'url': '//s3plus.sankuai.com/v1/mss_cf11584f34294f138b03f87308a62093/hqcc-batch-template/%E6%9D%A1%E7%A0%81%E5%8C%B9%E9%85%8D%E5%95%86%E5%93%81%E5%BA%93%E5%95%86%E5%93%81%E8%A1%A8%E6%A0%BC%20(20200807).xls',
+      'fields': ['upcCode', 'price', 'stock', 'tagName', 'subTagName', 'sourceFoodCode', 'locatorCode', 'minSaleCount', 'sellStatus', 'description', 'appFoodCode']
+    },
+    'hqccUpdateTpl': {
+      'meta': {
+        'lastModifyTime': 1603382400000,
+        'fields': 'matchTitle,matchUpc,matchSourceFoodCode,categoryId,title,originName,brandName,spec,price,stock,weight,weightUnit,upcCode,sourceFoodCode,tagName,subTagName,locatorCode,minSaleCount,sellStatus,description,appFoodCode',
+        'version': '1.1.6'
+      },
+      'lastModifyTime': -1,
+      'version': '1.1.6',
+      'url': '//s3plus.sankuai.com/v1/mss_cf11584f34294f138b03f87308a62093/hqcc-batch-template/%E6%89%B9%E9%87%8F%E4%BF%AE%E6%94%B9%E6%A8%A1%E7%89%88%EF%BC%8820201023%EF%BC%89.xls',
+      'fields': ['matchTitle', 'matchUpc', 'matchSourceFoodCode', 'categoryId', 'title', 'originName', 'brandName', 'spec', 'price', 'stock', 'weight', 'weightUnit', 'upcCode', 'sourceFoodCode', 'tagName', 'subTagName', 'locatorCode', 'minSaleCount', 'sellStatus', 'description', 'appFoodCode']
     }
-  }
+  }, 'code': 0, 'message': 'SUCCESS'
 }
