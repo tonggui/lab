4754c6b89b4749c7bec2e49e439975c7516c0f2a
袁帅 <yuanshuai15@meituan.com>
2021-01-22 14:51:09 +0800

    feat: 商品审核控制默认值

--- src/module/source.js
+++ src/module/source.js
@@ -139,7 +139,10 @@ const source = {
       }
       return isAuditApplyEnabled(context.poiId)
     },
-    defaultValue: {}
+    defaultValue: {
+      auditGray: false,
+      spAuditGray: false
+    }
   },
   poiAuditInfo: {
     fetch: () => fetchGetPoiAuditInfo(),
