c67de65d6ce1da181a4a8b49b47eeb346e013b80
liuyang271 <liuyang271@meituan.com>
2021-02-03 11:06:06 +0800

    feat: 修改任务进度 时间显示

--- src/data/api/medicineRegister.ts
+++ src/data/api/medicineRegister.ts
@@ -5,7 +5,7 @@ import httpClient from '../client/instance/medicineRegister'
 import {
   Pagination
 } from '@/data/interface/common'
-// import { formatTime } from '@/common/utils'
+import { formatTime } from '@/common/utils'
 
 import {
   MedicineRegisterSearchParams,
@@ -116,7 +116,7 @@ export const fetchTaskList = ({ pagination } : { pagination: Pagination }) => ht
       return {
         ...rest,
         name: taskName,
-        time: createTime,
+        time: formatTime(createTime),
         status: taskStatus,
         contentLink: ruleUrl,
         extraLink: failReasonUrl
