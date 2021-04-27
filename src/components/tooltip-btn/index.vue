1f7644491b974c9849fe4d17be565b387470d8a2
luxiaodong03 <luxiaodong03@meituan.com>
2021-04-27 16:03:51 +0800

    11334214 feat: 权限管理按钮

---
+++ src/components/tooltip-btn/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <Tooltip transfer :max-width="210" :content="desc" :disabled="!disabled">
+    <component :is="component" :type="type" @click="handleClick" :disabled="disabled">
+      <slot>新建</slot>
+    </component>
+  </Tooltip>
+</template>
+
+<script>
+  export default {
+    name: 'tooltip-btn',
+    props: {
+      desc: {
+        type: String,
+        default: '无此权限，请申请权限'
+      },
+      havePermission: {
+        type: Boolean,
+        default: true
+      },
+      type: {
+        type: String,
+        default: 'primary'
+      },
+      component: {
+        type: String,
+        default: 'Button'
+      }
+    },
+    data () {
+      return {
+        disabled: !this.havePermission,
+        urlsObj: {}
+      }
+    },
+    methods: {
+      handleClick () {
+        this.$emit('on-click')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/components/permission-bth/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <TooltipBtn
+    v-bind="$attrs"
+    v-on="$listeners"
+    desc="总部不允许分店使用此功能，如有疑问请您联系贵公司的总部工作人员"
+    :havePermission="havePermission"
+  >
+    <slot>新建</slot>
+  </TooltipBtn>
+</template>
+
+<script>
+  import TooltipBtn from '@components/tooltip-btn'
+  import { PERMISSION_TYPE_MAP } from '@/views/components/permission-bth/const'
+
+  export default {
+    name: 'PermissionBtn',
+    components: { TooltipBtn },
+    props: {
+      btnType: String,
+      permissionIdList: Array
+    },
+    computed: {
+      havePermission () {
+        return this.permissionIdList && this.permissionIdList.includes(PERMISSION_TYPE_MAP[this.btnType])
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
