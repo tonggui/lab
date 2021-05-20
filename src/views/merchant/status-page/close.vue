2842f9485e275bbff5e28a7ad65787896b3164a5
danzewei <danzewei@meituan.com>
2021-05-20 14:38:23 +0800

    feat: 接口交互优化

--- src/views/merchant/status-page/close.vue
+++ src/views/merchant/status-page/close.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="merchant-reset-container">
-    <div v-if="processing">
-      <Icon local="time" size="50" />
-      <p class="tip">关闭中</p>
-    </div>
-    <div v-else-if="fail" class="fail">
+    <div v-if="fail" class="fail">
       <Icon local="fail" size="50" />
       <p class="tip">网络异常，关闭失败</p>
       <p>
@@ -13,6 +9,10 @@
       <div class="footer">
         <Button type="primary" @click="handleJumpOut">跳过，直接进入"总部商品库"</Button>
       </div>
+    </div>
+    <div v-else>
+      <Icon local="time" size="50" />
+      <p class="tip">关闭中</p>
     </div>
   </div>
 </template>
@@ -54,7 +54,7 @@
             this.processing = true
           } else if (status === BATCH_REL_TASK_STATUS.FINISH) {
             if (BATCH_REL_TASK_RESULT_STATUS.ALL_SUCCESS === resultStatus) {
-              await fetchTaskFinish({ taskId: id })
+              await fetchTaskFinish(id)
               this.$router.push({ name: 'merchantList' })
             } else {
               this.id = id
