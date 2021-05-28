035a279f74970c36aa8776982e36a392bca57ed9
danzewei <danzewei@meituan.com>
2021-05-28 18:15:31 +0800

    feat: 商品分类选择组件替换、搜索列表页tagList透传

--- src/views/merchant/product/product-edit/form.js
+++ src/views/merchant/product/product-edit/form.js
@@ -1,6 +1,6 @@
 import createForm from '@/views/merchant/edit-page-common/form'
-// import createTagListPlugin from '@/views/components/configurable-form/plugins/tag-list'
-import createSuggestTagListPlugin from '@/views/components/configurable-form/plugins/suggest-tag-list'
+import createTagListPlugin from '@/views/components/configurable-form/plugins/checkbox-tag-list'
+// import createSuggestTagListPlugin from '@/views/components/configurable-form/plugins/suggest-tag-list'
 import createSuggestCategoryPlugin from '@/views/components/configurable-form/plugins/suggest-category'
 import createCategorySelectSpPlugin from '@/views/components/configurable-form/plugins/category-select-sp'
 import createSelectProductFromLibraryPlugin from '@/views/components/configurable-form/plugins/product-library'
@@ -10,13 +10,16 @@ import createProductCorrectionAuditTips
 
 import { fetchGetSuggestCategoryByProductName, fetchGetTagList } from '@/data/repos/merchantCategory'
 import { fetchGetCategoryAppealInfo } from '@/data/repos/merchantProduct'
-import { fetchGetSuggestTagInfo } from '@/data/repos/category'
+// import { fetchGetSuggestTagInfo } from '@/data/repos/category'
 
 const plugins = [
-  createSuggestTagListPlugin({
-    getTagList: fetchGetTagList,
-    getSuggestList: fetchGetSuggestTagInfo
+  createTagListPlugin({
+    getTagList: fetchGetTagList
   }),
+  // createSuggestTagListPlugin({
+  //   getTagList: fetchGetTagList,
+  //   getSuggestList: fetchGetSuggestTagInfo
+  // }),
   createCategorySelectSpPlugin(),
   createSelectProductFromLibraryPlugin(),
   createSuggestCategoryPlugin({
