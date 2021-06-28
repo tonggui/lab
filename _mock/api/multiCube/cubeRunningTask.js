219c0ca803236b7be86ae9c1d943f0d4b55284bd
danzewei <danzewei@meituan.com>
2021-06-28 20:00:32 +0800

    feat: mock数据修改

--- _mock/api/multiCube/cubeRunningTask.js
+++ _mock/api/multiCube/cubeRunningTask.js
@@ -5,29 +5,27 @@ module.exports = {
   code: 0,
   msg: '',
   data: {
-    cubeTaskStatus: {
-      taskId: -1,
-      taskName: '212',
-      status: 60,
-      runningStatusVo: {
-        runningStatus: 202,
-        mainSpuProcessStatusVo: {
-          finishedNum: 20,
-          totalNum: 20
-        },
-        poiAsyncProcessStatusVo: {
-          finishedNum: 10,
-          totalNum: 20
-        }
+    taskId: -1,
+    taskName: '212',
+    status: 60,
+    runningStatusVo: {
+      runningStatus: 202,
+      mainSpuProcessStatusVo: {
+        finishedNum: 20,
+        totalNum: 20
       },
-      finishStatusVo: {
-        finishStatus: 300,
-        succeedMainSpuNum: 10,
-        failedMainSpuNum: 14,
-        succeedMainSpuDetails: [],
-        failedMainSpuDetails: [],
-        failedUrl: '212121'
+      poiAsyncProcessStatusVo: {
+        finishedNum: 10,
+        totalNum: 20
       }
+    },
+    finishStatusVo: {
+      finishStatus: 300,
+      succeedMainSpuNum: 10,
+      failedMainSpuNum: 14,
+      succeedMainSpuDetails: [],
+      failedMainSpuDetails: [],
+      failedUrl: '212121'
     }
   }
 }
