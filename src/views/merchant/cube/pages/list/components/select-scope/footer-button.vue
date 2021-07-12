602ed8f866b90281e5b5cd0d4a2d6ad277e9e93f
guitong <guitong@meituan.com>
2021-07-12 13:52:54 +0800

    feat：表单校验

---
+++ src/views/merchant/cube/pages/list/components/select-scope/footer-button.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="footer-button">
+    <Button type="text" class="foot-btn-cancel" size="large" @click="$emit('cancel')">取消</Button>
+    <Button type="primary" class="foot-btn-confirm" size="large" @click="$emit('confirm')">确定</Button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'footer-button'
+  }
+</script>
+
+<style lang="less"  scoped>
+.footer-button{
+  line-height: 25px;
+  font-size: 12px;
+  font-weight: normal;
+  color: #222222;
+  .foot-btn-cancel{
+    border-color: #666666;
+    border-radius: 25px;
+  }
+  .foot-btn-confirm{
+    color: #222222;
+    border-color: antiquewhite;
+    background-image: linear-gradient(-45deg, #FFC34D 0%, #FFE14D 100%);
+    border-radius: 25px;
+  }
+}
+</style>
