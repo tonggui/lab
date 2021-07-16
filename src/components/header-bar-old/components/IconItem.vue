909436325f65c52a24d7261e4c3163be277c325a
danzewei <danzewei@meituan.com>
2021-07-16 11:26:42 +0800

    feat: 商品库顶部菜单消失问题修复

--- src/components/header-bar-old/components/IconItem.vue
+++ src/components/header-bar-old/components/IconItem.vue
@@ -10,7 +10,7 @@
   >
     <component :is="component" v-bind="tooltip">
       <template v-if="menu.children">
-        <Dropdown trigger="hover">
+        <Dropdown trigger="hover" :visible="menu.initVisible">
           <Icon :class="{ active: menu.active }" class="icon" v-bind="getIconProps(icon)">
             <component v-if="isComponent(icon)" :is="icon" />
             <Badge v-if="badgeProps" v-bind="badgeProps" />
@@ -20,15 +20,30 @@
             <Icon type="keyboard-arrow-down" />
           </div>
           <DropdownMenu slot="list">
-            <DropdownItem v-for="(subMenu, idx) in menu.children" :key="idx">
-              <RouteLink
-                class="download-item-link"
-                tag="a"
-                :to="subMenu.link||''"
-                :disabled="!!subMenu.disabled"
-                @click="handleClick($event, subMenu.bid)"
-              >{{subMenu.label}}
-              </RouteLink>
+            <DropdownItem v-for="(subMenu, idx) in menu.children" :key="idx" :id="subMenu.id">
+              <Tooltip type="guide" v-bind="subMenu.tooltip ? subMenu.tooltip : { disabled: true }">
+                <RouteLink
+                  v-if="!['批量新建', '批量修改', '批量传图'].includes(subMenu.label)"
+                  class="download-item-link"
+                  tag="a"
+                  :to="subMenu.link||''"
+                  :disabled="!!subMenu.disabled"
+                  @click="handleClick($event, subMenu.bid)"
+                >{{subMenu.label}}
+                </RouteLink>
+                <PermissionBtn
+                  v-else
+                  placement="left"
+                  component="RouteLink"
+                  :need-permission="needPermission"
+                  :btn-type="subMenu.label === '批量新建' ? 'CREATE' : 'EDIT'"
+                  className="download-item-link"
+                  tag="a"
+                  :to="subMenu.link||''"
+                  :disabled="!!subMenu.disabled"
+                  @click="handleClick($event, subMenu.bid)"
+                >{{subMenu.label}}</PermissionBtn>
+              </Tooltip>
             </DropdownItem>
           </DropdownMenu>
         </Dropdown>
@@ -53,69 +68,73 @@
     mixins: [menuItemMixins],
     components: {
       RouteLink
+    },
+    props: {
+      needPermission: Boolean
     }
   }
 </script>
 
 <style scoped lang="less">
-  .nav-link {
-    display: block;
-    cursor: pointer;
-    line-height: 16px;
-    text-align: center;
-    font-size: @font-size-base;
-    padding: 0;
-    margin-right: 16px;
-    color: @text-color;
-    &.disabled {
-      &,
-      .icon,
-      .icon.active {
-        color: @disabled-color;
-        cursor: not-allowed;
-      }
-      /deep/ .boo-badge-count {
-        background: @disabled-bg;
-        color: @disabled-color;
-      }
+.nav-link {
+  display: block;
+  cursor: pointer;
+  line-height: 16px;
+  text-align: center;
+  font-size: @font-size-base;
+  padding: 0;
+  margin-right: 16px;
+  color: @text-color;
+  &.disabled {
+    &,
+    .icon,
+    .icon.active {
+      color: @disabled-color;
+      cursor: not-allowed;
     }
-    .icon {
-      font-size: 32px;
-      color: @link-color;
-      margin-bottom: 5px;
-      position: relative;
-      &.active {
+    /deep/ .boo-badge-count {
+      background: @disabled-bg;
+      color: @disabled-color;
+    }
+  }
+  .icon {
+    font-size: 32px;
+    color: @link-color;
+    margin-bottom: 5px;
+    position: relative;
+    &.active {
+      color: @error-color;
+    }
+
+    .boo-badge {
+      position: absolute;
+      transform: translate(40%, -40%);
+      right: 0;
+      top: 0;
+
+      /deep/ .boo-badge-count {
+        height: 18px;
+        line-height: 16px;
+        padding: 0 4px;
+        font-weight: 700;
+      }
+      /deep/ .boo-badge-dot {
+        top: 2px;
+        right: -2px;
+      }
+
+      &.line /deep/ .boo-badge-count {
+        background: #fff;
         color: @error-color;
       }
-
-      .boo-badge {
-        position: absolute;
-        transform: translate(40%, -40%);
-        right: 0;
-        top: 0;
-
-        /deep/ .boo-badge-count {
-          height: 18px;
-          line-height: 16px;
-          padding: 0 4px;
-          font-weight: 700;
-        }
-        /deep/ .boo-badge-dot {
-          top: 2px;
-          right: -2px;
-        }
-
-        &.line /deep/ .boo-badge-count {
-          background: #fff;
-          color: @error-color;
-        }
-      }
     }
   }
-
-  .download-item-link {
-    color: @text-color;
-    font-size: @font-size-base;
-    padding: 0;
-  }
+}
+</style>
+<style lang="less">
+.download-item-link {
+  color: @text-color;
+  font-size: @font-size-base;
+  padding: 0;
+}
 </style>
