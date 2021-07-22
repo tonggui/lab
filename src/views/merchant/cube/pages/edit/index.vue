006368ddbf2b7c46421b6568c2ffc0b0eb48d135
danzewei <danzewei@meituan.com>
2021-07-22 16:28:37 +0800

    feat: 编辑页操作栏固定样式优化、toast提示优化等

--- src/views/merchant/cube/pages/edit/index.vue
+++ src/views/merchant/cube/pages/edit/index.vue
@@ -33,11 +33,9 @@
         this.$router.back()
       },
       handleExistToast () {
-        console.log('wqwqwqwqwq', this.tagGroupProduct)
         const hasHqExist = Object.values(this.tagGroupProduct || {}).some(item => (item.productList || []).some(pro => !!pro.isHqExist))
         if (hasHqExist) {
-          this.$Modal.info({
-            title: '温馨提示',
+          this.$Message.info({
             content: '选中商品若为总部已存在，直接基于现有商品信息进行编辑完善'
           })
         }
